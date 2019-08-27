a58cb15ca008f72c49a6b56632443cc0f322e73e
zhangyong19800721 <24452861@qq.com>
2019-08-27 14:50:31 +0800

    update

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ voc = pickle.load(voc_file)
 voc_file.close()
 
 trainset = DATASET.GCDYW(r"./data/corpus_trainset_digit.cps")  # 加载训练数据
-dataloader = DATASET.LOADER(trainset, minibatch_size=20)  # 数据加载器，设定minibatch的大小
+minibatch_size = 20
+dataloader = DATASET.LOADER(trainset, minibatch_size=minibatch_size)  # 数据加载器，设定minibatch的大小
 
-embedding_dim = 128  # 词向量的维度
+embedding_dim = 128
 hidden_size = 256
 num_layers = 2
+
 word_embedding = nn.Embedding(voc.num_words, embedding_dim)  # 初始化词向量
 model = MODEL.ArticleReviewer(embedding_dim, hidden_size, word_embedding, num_layers)
 
@@ -33,10 +35,11 @@ start_time = time.time()
 model.to(device)  # 将模型推入GPU
 
 lossList = []
-exponentiaAveLoss = 1
+expAveLoss = 0
 
 minibatch_num = len(dataloader)
-for epoch in range(50):
+epoch_num = 50
+for epoch in range(epoch_num):
     for minibatch_id in range(minibatch_num):
         minibatch = dataloader[minibatch_id]
         datas = minibatch['article'].to(device)  # 将数据推送到GPU
@@ -47,18 +50,17 @@ for epoch in range(50):
         lossList.append(loss)
         while len(lossList) > minibatch_num:
             lossList.pop(0)
-        exponentiaAveLoss = 99 / 100 * exponentiaAveLoss + 1 / 100 * loss
+        expAveLoss = 99 / 100 * expAveLoss + 1 / 100 * loss
         aveLoss = sum(lossList) / len(lossList)  # 计算平均损失函数
-        print("epoch:%5d, minibatch_id:%5d/%d, expAveLoss:%10.8f, aveloss:%10.8f" % (epoch, minibatch_id, minibatch_num, exponentiaAveLoss, aveLoss))
+        print("epoch:%5d/%d, minibatch_id:%5d/%d, expAveLoss:%10.8f, aveloss:%10.8f" % (epoch, epoch_num, minibatch_id, minibatch_num, expAveLoss, aveLoss))
         loss.backward()  # 反向传播
         optimizer.step()  # 更新参数
 
     # save model every epoch
-    model_file = open(r"./model/model%03d.pkl"%epoch, "wb")
+    model_file = open(r"./model/model%03d.pkl" % epoch, "wb")
     pickle.dump(model, model_file)
     model_file.close()
 
 end_time = time.time()
+
 print(f'train_time = {(end_time - start_time) / 60} min')
-
-
